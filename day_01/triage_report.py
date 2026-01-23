091fa03a9b51529538ec33375caa5575b7b6641f
saorsh-asjadullah <sarosh.asjadullah@gmail.com>
2026-01-23 03:21:50 +0300

    Update triage_report.py

--- day_01/triage_report.py
+++ day_01/triage_report.py
@@ -1,1 +1,6 @@
+analyst_name:str = input("\nEnter the analyst name: ").strip()
+case_number:str = input("\nEnter the Case number: ").strip()
+ip_address:str = input("\nEnter the target ip address: ").strip()
 
+output = f"[REPORT] Analyst: {analyst_name} | Case: {case_number} | Target:{ip_address}"
+print(output)
