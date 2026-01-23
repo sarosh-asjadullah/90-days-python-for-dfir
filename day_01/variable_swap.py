c5b43b119b588a6bf430da0e2c45b881e973b24e
saorsh-asjadullah <sarosh.asjadullah@gmail.com>
2026-01-23 03:22:30 +0300

    Initial logic

--- day_01/variable_swap.py
+++ day_01/variable_swap.py
@@ -1,1 +1,9 @@
+primary_dns ="8.8.8.8"
+secondary_dns = "1.1.1.1"
 
+print("\nThe Primary DNS is",primary_dns + " and The Secondary DNS is",secondary_dns)
+
+primary_dns = "9.9.9.9"
+secondary_dns = "8.8.4.4"
+
+print("\nThe Primary DNS is",primary_dns + " and The Secondary DNS is",secondary_dns)
